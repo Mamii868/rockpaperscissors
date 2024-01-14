44a409a7da44a27ce9b27310c7ef99f126f03b8b
Mamii868 <marcmotenii@gmail.com>
2024-01-14 14:30:19 -0600

    add return of choice
    moved the choice variable to global scope to be modified in the function, returning rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
+let choice;
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1
+    choice = Math.floor(Math.random() * 3) + 1
     console.log(choice)
+    if (choice === 1) {
+        choice = 'rock'
+    } else if (choice === 2) {
+        choice = 'paper'
+    } else {
+        choice = 'scissors'
+    }
+    return choice
 }
 
+
 getComputerChoice()
+console.log(choice)
