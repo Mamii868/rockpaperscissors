c365528a95c655c019fe01ec658d4f2a4491d0b8
Mamii868 <marcmotenii@gmail.com>
2024-01-17 05:05:54 -0600

    add logic for choosing the winner
    runs through a list of conditionals to determine the outcome.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let choice;
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3) + 1
+    //Remove
     console.log(choice)
     if (choice === 1) {
         choice = 'rock'
@@ -15,4 +16,18 @@ function getComputerChoice() {
 
 
 getComputerChoice()
+//Remove
 console.log(choice)
+
+function round(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        alert("it's a tie! Try again!")
+        getPlayerChoice();
+    } else if (playerSelection === 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors') {
+        alert('You Lose! Try again.')
+        getPlayerChoice();
+    } else {
+        alert('You Win!')
+    }
+}
