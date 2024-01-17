2814c4ca3616d60d4f38e4a633d0da9abe43a26c
Mamii868 <marcmotenii@gmail.com>
2024-01-17 05:28:06 -0600

    Add catch for wrong input
    game now checks for the right input in prompt inside the round function. file formatted with prettier.

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-let playerSelection
+let playerSelection;
 let computerSelection;
 function getComputerChoice() {
-    computerSelection = Math.floor(Math.random() * 3) + 1
-    //Remove
-    console.log(computerSelection)
-    if (computerSelection === 1) {
-        computerSelection = 'rock'
-    } else if (computerSelection === 2) {
-        computerSelection = 'paper'
-    } else {
-        computerSelection = 'scissors'
-    }
-    return computerSelection
+  computerSelection = Math.floor(Math.random() * 3) + 1;
+  //Remove
+  console.log(computerSelection);
+  if (computerSelection === 1) {
+    computerSelection = "rock";
+  } else if (computerSelection === 2) {
+    computerSelection = "paper";
+  } else {
+    computerSelection = "scissors";
+  }
+  return computerSelection;
 }
 
-
-getComputerChoice()
+getComputerChoice();
 //Remove
-console.log(computerSelection)
+console.log(computerSelection);
 
 function getPlayerChoice() {
-
-playerSelection = prompt("Rock, Paper, or Scissors?")
-console.log(playerSelection)
-round(playerSelection, computerSelection);
+  playerSelection = prompt("Rock, Paper, or Scissors?");
+  console.log(playerSelection);
+  round(playerSelection, computerSelection);
 }
 function round(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        alert("it's a tie! Try again!")
-        getPlayerChoice();
-    } else if (playerSelection === 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors') {
-        alert('You Lose! Try again.')
-        getPlayerChoice();
-    } else {
-        alert('You Win!')
-    }
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    alert("it's a tie! Try again!");
+    getPlayerChoice();
+  } else if (
+    (playerSelection === "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors")
+  ) {
+    alert("You Lose! Try again.");
+    getPlayerChoice();
+  } else if (
+    playerSelection != "rock" ||
+    playerSelection != "paper" ||
+    playerSelection != "scissors"
+  ) {
+    alert("Please enter the right item!");
+    getPlayerChoice();
+  } else {
+    alert("You Win!");
+  }
 }
 
-getPlayerChoice()
+getPlayerChoice();
