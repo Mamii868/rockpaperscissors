08eab24bccb5e2cce88d18a89d8a6df3e53f5dab
Mamii868 <marcmotenii@gmail.com>
2024-02-07 18:47:15 -0600

    Add restart logic

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let playerSelection;
 let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
-const button = document.querySelectorAll("button");
+const button = document.querySelectorAll(".choice");
 const resultsContainer = document.querySelector(".results");
 const messageBox = document.querySelector(".message");
+const finalBox = document.querySelector(".final");
+const restart = document.querySelector(".restart");
 
 //Logic for computer opponent
 function getComputerChoice() {
@@ -31,14 +33,14 @@ button.forEach((btn) =>
 function round(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    messageBox.innerHTML = "It's a tie! Run it back."
+    messageBox.innerHTML = "It's a tie! Run it back.";
     getComputerChoice();
   } else if (
     (playerSelection === "rock" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "rock")
   ) {
-    messageBox.innerHTML = "Computer Won. Try Again!"
+    messageBox.innerHTML = "Computer Won. Try Again!";
     computerScore++;
     getComputerChoice();
   } else if (
@@ -46,17 +48,20 @@ function round(playerSelection, computerSelection) {
     playerSelection != "paper" &&
     playerSelection != "scissors"
   ) {
-    messageBox.innerHTML = "You should never see this error"
+    messageBox.innerHTML = "You should never see this error";
   } else {
-    messageBox.innerHTML = "You won the round! Keep going!"
-    playerScore++
+    messageBox.innerHTML = "You won the round! Keep going!";
+    playerScore++;
     getComputerChoice();
   }
 }
 
 if (playerScore === 5) {
-  alert("You Win! Press OK to try again!")
-  location.reload()
+  finalBox.innerHTML = "You Win! Press the button to play again!";
 } else if (computerScore === 5) {
-  alert("You Lose! Press OK to try again!")
+  finalBox.innerHTML = "You Lose! Press the button to play again!";
 }
+
+restart.addEventListener("click", () => {
+  location.reload();
+});
